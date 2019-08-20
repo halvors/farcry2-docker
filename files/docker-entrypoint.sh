67d06a4b59f1e51c2899f00ad148d165a76a23d1
halvors <halvors@halvors.org>
2019-08-21 00:39:42 +0200

    Made entrypoint executable.

--- files/docker-entrypoint.sh
+++ files/docker-entrypoint.sh
@@ -1,19 +1,7 @@
 #!/bin/bash
 set -eoux pipefail
 
-FACTORIO_VOL=/factorio
-
 # shellcheck disable=SC2086
-exec $SU_EXEC /opt/factorio/bin/x64/factorio
-#  --port "$PORT" \
-#  --start-server-load-latest \
-#  --server-settings "$CONFIG/server-settings.json" \
-#  --server-banlist "$CONFIG/server-banlist.json" \
-#  --rcon-port "$RCON_PORT" \
-#  --server-whitelist "$CONFIG/server-whitelist.json" \
-#  --use-server-whitelist \
-#  --server-adminlist "$CONFIG/server-adminlist.json" \
-#  --rcon-password "$(cat "$CONFIG/rconpw")" \
-#  --server-id /factorio/config/server-id.json \
+exec /opt/farcry2/bin/FarCry2_server \
   "$@"
 

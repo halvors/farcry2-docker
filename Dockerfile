0deb349c12412bc77cacf62189667b92af787844
halvors <halvors@halvors.org>
2019-08-22 04:49:17 +0200

    Updated docker container to properly load config from data dir.

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,10 @@ ARG PUID=845
 ARG PGID=845
 
 ENV SHA256=281e69fc0cccfa4760ba8db3b82315f52d2f090d9d921dc3adc89afbf046898a \
+    CONFIG=/farcry2/config \
+    MAPS=/farcry2/user\ maps \
+    USER="$USER" \
+    GROUP="$GROUP" \
     PUID="$PUID" \
     PGID="$PGID"
 
@@ -26,13 +30,15 @@ RUN set -x && \
     mv $directory /opt/farcry2 && \
     chmod ugo=rwx /opt/farcry2 && \
     rm "$archive" && \
+    #ln -s "$MAPS" /opt/farcry2/user\ maps && \
+    #ln -s "$MAPS" /opt/farcry2/bin/user\ maps && \
     groupadd -g "$PGID" "$GROUP" && \
     useradd -u "$PUID" -g "$GROUP" -s /bin/sh "$USER" && \
-    chown -R "$USER":"$GROUP" /opt/farcry2 /farcry2
+    chown -R "$USER":"$GROUP" /opt/farcry2 /farcry2 && \
 
 VOLUME /farcry2
  
-EXPOSE 9000-9003/udp 9000-9003/tcp
+EXPOSE 9000-9003/udp 9000/tcp
 
 COPY files/ /
 
--- files/docker-entrypoint.sh
+++ files/docker-entrypoint.sh
@@ -3,13 +3,20 @@ set -eoux pipefail
 
 VOLUME=/farcry2
 mkdir -p "$VOLUME"
+mkdir -p "$CONFIG"
+mkdir -p "$MAPS"
+
+if [[ ! -f $CONFIG/server.cfg ]]; then
+  # Copy default settings if server.cfg doesn't exist
+  cp /server.cfg "$CONFIG/server.cfg"
+fi
 
 if [[ $(id -u) = 0 ]]; then
   # Update the User and Group ID based on the PUID/PGID variables
-  usermod -o -u "$PUID" farcry2
-  groupmod -o -g "$PGID" farcry2
+  usermod -o -u "$PUID" "$USER"
+  groupmod -o -g "$PGID" "$GROUP"
   # Take ownership of farcry2 data if running as root
-  chown -R farcry2:farcry2 "$VOLUME"
+  chown -R "$USER":"$GROUP" "$VOLUME"
 
   # Drop to the farcry2 user
 #  SU_EXEC="su farcry2 -c"
@@ -20,5 +27,5 @@ fi
 # shellcheck disable=SC2086
 #exec $SU_EXEC /opt/farcry2/bin/FarCry2_server \
 exec /opt/farcry2/bin/FarCry2_server \
-#  --dedicated /server.cfg
+  --dedicated /farcry2/config/server.cfg
   "$@"
